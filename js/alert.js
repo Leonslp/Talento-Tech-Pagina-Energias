f6c2798613e00005867b42b59c40eba0d3da420b
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2025-02-26 19:33:14 -0500

    Cambios al index
    - Color al boton de desplazamiento
    - Imagenes y contenido
    - creacion de alerta

---
+++ js/alert.js
@@ -0,0 +1,70 @@
+function object1(){
+  Swal.fire({
+    html: "Disminuir la cantidad de CO2 y otros contaminantes liberados en la atmósfera.",
+    showButtonText: true,
+    confirmButtonText: 'X',
+    confirmButtonColor: 'green',
+    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("¡¡¡Si!!!");
+    }
+  });
+}
+
+
+function object2(){
+  Swal.fire({
+    html: "Incrementar el uso de fuentes renovables y tecnologías eficientes.",
+    showButtonText: true,
+    confirmButtonText: 'X',
+    confirmButtonColor: 'green',
+    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("¡¡¡Si!!!");
+    }
+  });
+}
+
+function object3(){
+  Swal.fire({
+    html: "Eliminar progresivamente el uso de combustibles fósiles en la generación de energía y en sectores industriales. ",
+    showButtonText: true,
+    confirmButtonText: 'X',
+    confirmButtonColor: 'green',
+    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("¡¡¡Si!!!");
+    }
+  });
+}
+
+function object4(){
+  Swal.fire({
+    html: "Mejorar el uso de la energía para reducir el desperdicio y optimizar los recursos disponibles. ",
+    showButtonText: true,
+    confirmButtonText: 'X',
+    confirmButtonColor: 'green',
+    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("¡¡¡Si!!!");
+    }
+  });
+}
+
+function object5(){
+  Swal.fire({
+    html: "Garantizar que todas las comunidades tengan acceso a energía limpia y asequible.",
+    showButtonText: true,
+    confirmButtonText: 'X',
+    confirmButtonColor: 'green',
+    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("¡¡¡Si!!!");
+    }
+  });
+}
